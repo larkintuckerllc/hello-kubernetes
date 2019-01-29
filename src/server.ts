c0405f53a56498b50b881490610b521a5856947b
John Tucker <jtucker@appirio.com>
2019-01-29 06:49:48 -0500

    Fixed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import cors from 'cors';
 import express from 'express';
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import Todo from './entity/Todo';
 
-const app = express();
-app.use(cors());
-app.get('/', (req, res) => res.send({ hello: 'world' }));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+createConnection()
+  .then(async connection => {
+    const app = express();
+    app.use(cors());
+    app.get('/', (req, res) => res.send({ hello: 'world' }));
+    app.get('/create', async (req, res) => {
+      const todo = new Todo();
+      todo.name = 'A Todo';
+      await connection.manager.save(todo);
+      res.send(todo);
+    });
+    app.get('/read', async (req, res) => {
+      const todos = await connection.manager.find(Todo);
+      res.send(todos);
+    });
+    app.listen(3000, () => console.log('Example app listening on port 3000!'));
+  })
+  .catch(error => console.log(error));
